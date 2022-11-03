c2fb6dd9c619437f2a58d50887893c7d2d21ec88
paolod <paolodellepiane@proton.me>
2022-11-03 21:32:53 +0100

    improve win release script

--- scripts/release-win.ps1
+++ scripts/release-win.ps1
@@ -1,6 +1,11 @@
-param ($version)
-cargo build --release
+$initialdir = pwd
+cd $PSScriptRoot\..
+$version = (cat ./Cargo.toml|Select-String -Pattern  '^version = \"(\d+\.\d+\.\d+)\"$').Matches.Groups[1].Value
+if (Test-Path release) {
+    rm -Recurse -Force release
+}
 mkdir release
+cargo build --release
 cp target\release\ash.exe release\
 cp scripts\ash.json release\ash.json
 cd release
@@ -8,5 +13,5 @@ Compress-Archive ash.exe ash-$version-win-x64.zip
 (Get-FileHash .\ash-$version-win-x64.zip).Hash > .\ash-$version-win-x64.zip.sha256
 gh release create v$version --generate-notes .\ash-$version-win-x64.zip .\ash-$version-win-x64.zip.sha256
 & $env:UserProfile\scoop\apps\scoop\current\bin\checkver.ps1 ash . -Update
-cat ash.json | gh gist edit 80e7005c4fd62cec9161f74bc2ad24ff -f ash.json -
-cd ..
+cat ash.json | gh gist edit a059f56c8026d8581add2bc29f80f144 -f ash.json -
+cd $initialdir

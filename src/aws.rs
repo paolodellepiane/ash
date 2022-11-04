fe7afd5df9a69767b102b51ebf70c07557f8f90b
paolod <paolodellepiane@proton.me>
2022-11-04 12:04:03 +0100

    add xml ext

--- src/aws.rs
+++ src/aws.rs
@@ -7,6 +7,7 @@ use http::Request;
 use ini::Ini;
 use itertools::Itertools;
 use minreq::{Response, URL};
+use roxmltree::Node;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env::var;
@@ -34,6 +35,16 @@ struct Credential {
     expiration: String,
 }
 
+trait NodeExt {
+    fn find_tag(&self, name: &str) -> Option<Node>;
+}
+
+impl<'a, 'input> NodeExt for Node<'a, 'input> {
+    fn find_tag(&self, name: &str) -> Option<Node> {
+        self.children().find(|t| t.has_tag_name(name))
+    }
+}
+
 impl Credential {
     fn is_expired(&self) -> bool {
         let Ok(exp) = chrono::DateTime::parse_from_rfc3339(&self.expiration) else {
@@ -51,17 +62,19 @@ struct AwsConfig {
     region: String,
 }
 
-fn aws_sign(req: &mut Request<impl AsRef<[u8]>>,
-            service_name: &str,
-            Credential { access_key, secret, token, region, .. }: &Credential)
-            -> Result<()> {
+fn aws_sign(
+    req: &mut Request<impl AsRef<[u8]>>,
+    service_name: &str,
+    Credential { access_key, secret, token, region, .. }: &Credential,
+) -> Result<()> {
     let signing_settings = SigningSettings::default();
-    let mut signing_params = SigningParams::builder().access_key(access_key)
-                                                     .secret_key(secret)
-                                                     .region(region)
-                                                     .service_name(service_name)
-                                                     .time(SystemTime::now())
-                                                     .settings(signing_settings);
+    let mut signing_params = SigningParams::builder()
+        .access_key(access_key)
+        .secret_key(secret)
+        .region(region)
+        .service_name(service_name)
+        .time(SystemTime::now())
+        .settings(signing_settings);
     if !token.is_empty() {
         signing_params = signing_params.security_token(token)
     }
@@ -86,47 +99,56 @@ fn aws_send(url: &str, service_name: &str, cred: &Credential) -> Result<Response
     Ok(res)
 }
 
-fn get_sts_creds(AwsConfig { profile, role_arn, region, .. }: AwsConfig,
-                 cred: &Credential)
-                 -> Result<Credential> {
+fn get_sts_creds(
+    AwsConfig { profile, role_arn, region, .. }: AwsConfig,
+    cred: &Credential,
+) -> Result<Credential> {
     let assume_role = f!("https://sts.{region}.amazonaws.com/?Version=2011-06-15&Action=AssumeRole&RoleSessionName={profile}&RoleArn={role_arn}&DurationSeconds=3600");
     let res = aws_send(&assume_role, "sts", cred)?;
     if res.status_code < 200 || res.status_code > 299 {
-        bail!("Error assuming role for {profile}, {}, {}",
-              res.status_code,
-              res.reason_phrase)
+        bail!(
+            "Error assuming role for {profile}, {}, {}",
+            res.status_code,
+            res.reason_phrase
+        )
     }
     let doc = roxmltree::Document::parse(res.as_str()?)?;
-    let access_key = doc.descendants()
-                        .find(|n| n.has_tag_name("AccessKeyId"))
-                        .and_then(|x| x.text())
-                        .ok_or_else(|| anyhow!("can't get access_key for {profile}"))?
-                        .to_string();
-    let secret = doc.descendants()
-                    .find(|n| n.has_tag_name("SecretAccessKey"))
-                    .and_then(|x| x.text())
-                    .ok_or_else(|| anyhow!("can't get secret for {profile}"))?
-                    .to_string();
-    let token = doc.descendants()
-                   .find(|n| n.has_tag_name("SessionToken"))
-                   .and_then(|x| x.text())
-                   .ok_or_else(|| anyhow!("can't get token for {profile}"))?
-                   .to_string();
-    let expiration = doc.descendants()
-                        .find(|n| n.has_tag_name("Expiration"))
-                        .and_then(|x| x.text())
-                        .ok_or_else(|| anyhow!("can't get expiration for {profile}"))?
-                        .to_string();
+    let access_key = doc
+        .descendants()
+        .find(|n| n.has_tag_name("AccessKeyId"))
+        .and_then(|x| x.text())
+        .ok_or_else(|| anyhow!("can't get access_key for {profile}"))?
+        .to_string();
+    let secret = doc
+        .descendants()
+        .find(|n| n.has_tag_name("SecretAccessKey"))
+        .and_then(|x| x.text())
+        .ok_or_else(|| anyhow!("can't get secret for {profile}"))?
+        .to_string();
+    let token = doc
+        .descendants()
+        .find(|n| n.has_tag_name("SessionToken"))
+        .and_then(|x| x.text())
+        .ok_or_else(|| anyhow!("can't get token for {profile}"))?
+        .to_string();
+    let expiration = doc
+        .descendants()
+        .find(|n| n.has_tag_name("Expiration"))
+        .and_then(|x| x.text())
+        .ok_or_else(|| anyhow!("can't get expiration for {profile}"))?
+        .to_string();
 
     Ok(Credential { access_key, secret, token, profile, region, expiration })
 }
 
 fn get_env_credentials() -> Result<Credential> {
-    Ok(Credential { profile: "default".into(),
-                    access_key: var("AWS_ACCESS_KEY_ID")?,
-                    secret: var("AWS_SECRET_ACCESS_KEY")?,
-                    region: var("AWS_DEFAULT_REGION")?,
-                    ..Credential::default() })
+    Ok(Credential {
+        profile: "default".into(),
+        access_key: var("AWS_ACCESS_KEY_ID")?,
+        secret: var("AWS_SECRET_ACCESS_KEY")?,
+        region: var("AWS_DEFAULT_REGION")?,
+        ..Credential::default()
+    })
 }
 
 fn get_shared_credentials() -> Result<Vec<Credential>> {
@@ -135,43 +157,41 @@ fn get_shared_credentials() -> Result<Vec<Credential>> {
     let aws_config = user_dirs.home_dir().join(".aws").join("config");
     let confs: HashMap<_, _> = if aws_config.exists() {
         let config_ini = Ini::load_from_file(&aws_config).context("Can't load aws config")?;
-        config_ini.iter()
-                  .map(|(sec, props)| {
-                      let mut profile = sec.unwrap().to_string();
-                      if profile.starts_with("profile ") {
-                          profile = profile.strip_prefix("profile ").unwrap().to_string();
-                      }
-                      let region = props.get("region").unwrap_or_default().to_string();
-                      let role_arn = props.get("role_arn").unwrap_or_default().to_string();
-                      let source_profile =
-                          props.get("source_profile").unwrap_or_default().to_string();
-                      (profile.clone(), AwsConfig { profile, region, role_arn, source_profile })
-                  })
-                  .collect()
+        config_ini
+            .iter()
+            .map(|(sec, props)| {
+                let mut profile = sec.unwrap().to_string();
+                if profile.starts_with("profile ") {
+                    profile = profile.strip_prefix("profile ").unwrap().to_string();
+                }
+                let region = props.get("region").unwrap_or_default().to_string();
+                let role_arn = props.get("role_arn").unwrap_or_default().to_string();
+                let source_profile = props.get("source_profile").unwrap_or_default().to_string();
+                (
+                    profile.clone(),
+                    AwsConfig { profile, region, role_arn, source_profile },
+                )
+            })
+            .collect()
     } else {
         HashMap::new()
     };
 
     // todo: expand source profiles
-    let mut creds: HashMap<_, _> =
-        Ini::load_from_file(&aws_credentials).context("Can't load aws credentials")?
-                                             .iter()
-                                             .filter_map(|(sec, props)| {
-                                                 let profile = sec?.to_string();
-                                                 let access_key =
-                                                     props.get("aws_access_key_id")?.to_string();
-                                                 let secret = props.get("aws_secret_access_key")?
-                                                                   .to_string();
-                                                 let region = confs.get(&profile)
-                                                                   .map(|c| c.region.clone())?;
-                                                 Some((profile.clone(),
-                                                       Credential { profile,
-                                                                    access_key,
-                                                                    secret,
-                                                                    region,
-                                                                    ..Credential::default() }))
-                                             })
-                                             .collect();
+    let mut creds: HashMap<_, _> = Ini::load_from_file(&aws_credentials)
+        .context("Can't load aws credentials")?
+        .iter()
+        .filter_map(|(sec, props)| {
+            let profile = sec?.to_string();
+            let access_key = props.get("aws_access_key_id")?.to_string();
+            let secret = props.get("aws_secret_access_key")?.to_string();
+            let region = confs.get(&profile).map(|c| c.region.clone())?;
+            Some((
+                profile.clone(),
+                Credential { profile, access_key, secret, region, ..Credential::default() },
+            ))
+        })
+        .collect();
 
     if let Ok(cache) = File::open(Config::cache_path()) {
         if let Ok(cached_creds) = serde_json::from_reader::<File, Vec<Credential>>(cache) {
@@ -205,10 +225,11 @@ fn get_shared_credentials() -> Result<Vec<Credential>> {
 
     let tosave = creds.values().filter(|x| !x.token.is_empty()).collect_vec();
     match serde_json::to_string(&tosave) {
-        Ok(json) =>
+        Ok(json) => {
             if let Err(err) = std::fs::write(Config::cache_path(), json) {
                 p!("Can't write cache: {err:#}");
-            },
+            }
+        }
         Err(err) => p!("Can't serialize cache; {err:#}"),
     }
 
@@ -222,85 +243,76 @@ fn get_credentials() -> Result<Vec<Credential>> {
     get_shared_credentials()
 }
 
-fn update_from_aws_api(keys_path: impl AsRef<Path>,
-                       cred: &Credential,
-                       proxy_jump: Option<&str>)
-                       -> Result<Vec<Instance>> {
+fn update_from_aws_api(
+    keys_path: impl AsRef<Path>,
+    cred: &Credential,
+    proxy_jump: Option<&str>,
+) -> Result<Vec<Instance>> {
     let region = &cred.region;
     let describe_instances = f!("https://ec2.{region}.amazonaws.com/?Action=DescribeInstances&Version=2016-11-15&Filter.1.Name=instance-state-name&Filter.1.Value.1=running");
     let res = aws_send(&describe_instances, "ec2", cred)?;
     if res.status_code == 401 {
         std::fs::remove_file(Config::cache_path())?;
-        bail!("Authorization failed for profile {}. Credential cache cleared. Please retry",
-              cred.profile)
+        bail!(
+            "Authorization failed for profile {}. Credential cache cleared. Please retry",
+            cred.profile
+        )
     }
     if res.status_code < 200 || res.status_code > 299 {
-        bail!("Error getting instances for {}, {}, {}",
-              cred.profile,
-              res.status_code,
-              res.reason_phrase)
+        bail!(
+            "Error getting instances for {}, {}, {}",
+            cred.profile,
+            res.status_code,
+            res.reason_phrase
+        )
     }
     let doc = roxmltree::Document::parse(res.as_str()?)?;
-    let instances =
-        doc.descendants()
-           .filter(|n| n.has_tag_name("instancesSet"))
-           .filter_map(|i| {
-               let instance = i.first_element_child()?;
-               let key = instance.children().find(|x| x.has_tag_name("keyName"))?.text()?;
-               let key = keys_path.as_ref().join(key).to_str()?.to_string();
-               let address = if proxy_jump.unwrap_or_default().is_empty() {
-                   instance.children().find(|x| x.has_tag_name("ipAddress"))?.text()?.to_string()
-               } else {
-                   instance.children()
-                           .find(|x| x.has_tag_name("privateIpAddress"))?
-                           .text()?
-                           .to_string()
-               };
-               let mut tag_set_items =
-                   instance.children().find(|x| x.has_tag_name("tagSet"))?.children();
-               let tag_name = tag_set_items.find(|x| {
-                                               x.children().any(|x| {
-                                                               x.has_tag_name("key")
-                                                               && x.text().unwrap_or_default()
-                                                                  == "Name"
-                                                           })
-                                           })?;
-               let name = tag_name.children()
-                                  .find(|x| x.has_tag_name("value"))?
-                                  .text()?
-                                  .to_string()
-                                  .replace(' ', "-");
-               let platform = instance.children()
-                                      .find(|x| x.has_tag_name("platformDetails"))?
-                                      .text()?
-                                      .to_string();
-               Some(Instance { name,
-                               key,
-                               address,
-                               platform,
-                               profile: cred.profile.to_owned(),
-                               proxy_jump: proxy_jump.map(String::from) })
-           })
-           .collect_vec();
+    let instances = doc
+        .descendants()
+        .filter(|n| n.has_tag_name("instancesSet"))
+        .filter_map(|i| {
+            let instance = i.first_element_child()?;
+            let key = instance.find_tag("keyName")?.text()?;
+            let key = keys_path.as_ref().join(key).to_str()?.to_string();
+            let address_tag = if proxy_jump.is_falsy() { "ipAddress" } else { "privateIpAddress" };
+            let address = instance.find_tag(address_tag)?.text()?.to_string();
+            let mut tag_set_items = instance.find_tag("tagSet")?.children();
+            let tag_name =
+                tag_set_items.find_map(|x| (x.find_tag("key")?.text()? == "Name").then_some(x))?;
+            let name = tag_name.find_tag("value")?.text()?;
+            let name = name.to_string().replace(' ', "-");
+            let platform = instance.find_tag("platformDetails")?.text()?.to_string();
+            Some(Instance {
+                name,
+                key,
+                address,
+                platform,
+                profile: cred.profile.to_owned(),
+                proxy_jump: proxy_jump.map(String::from),
+            })
+        })
+        .collect_vec();
     Ok(instances)
 }
 
-pub fn update_sshconfig(keys_path: impl AsRef<Path>,
-                        template: impl AsRef<Path>,
-                        proxy_jump: Option<&str>)
-                        -> Result<()> {
+pub fn update_sshconfig(
+    keys_path: impl AsRef<Path>,
+    template: impl AsRef<Path>,
+    proxy_jump: Option<&str>,
+) -> Result<()> {
     let keys_path = keys_path.as_ref();
     let mut srvs: Vec<Instance> = Vec::new();
     let credentials = &get_credentials().context("No credentials found")?;
     ensure!(!credentials.is_empty(), "No credentials found");
     thread::scope(|scope| {
-        let threads: Vec<_> = credentials.iter()
-                                         .map(|c| {
-                                             scope.spawn(move || {
+        let threads: Vec<_> = credentials
+            .iter()
+            .map(|c| {
+                scope.spawn(move || {
                     update_from_aws_api(keys_path, c, proxy_jump).context(c.profile.clone())
                 })
-                                         })
-                                         .collect();
+            })
+            .collect();
         for t in threads {
             match &mut t.join() {
                 Ok(Ok(add)) => srvs.append(add),
@@ -311,10 +323,11 @@ pub fn update_sshconfig(keys_path: impl AsRef<Path>,
     });
     let tmpl = std::fs::read_to_string(template)?;
     let res = Handlebars::new().render_template(&tmpl, &to_json(srvs))?;
-    let ssh_config = directories::UserDirs::new().context("can't retrieve home directory")?
-                                                 .home_dir()
-                                                 .join(".ssh")
-                                                 .join("config");
+    let ssh_config = directories::UserDirs::new()
+        .context("can't retrieve home directory")?
+        .home_dir()
+        .join(".ssh")
+        .join("config");
     std::fs::write(ssh_config, res)?;
 
     Ok(())
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 pub use crate::option_not_empty_string::*;
 pub use anyhow::{anyhow, bail, ensure, Context, Result};
 pub use std::format as f;
+use std::ops::Deref;
 pub use std::println as p;
 use std::time::Instant;
 
@@ -9,7 +10,7 @@ pub trait ThrutyOptionStringExt {
     fn is_truthy(&self) -> bool;
 }
 
-impl ThrutyOptionStringExt for Option<String> {
+impl<S> ThrutyOptionStringExt for Option<S> where S: Deref<Target = str> {
     fn is_falsy(&self) -> bool {
         self.is_none() || self.as_ref().unwrap().is_empty()
     }

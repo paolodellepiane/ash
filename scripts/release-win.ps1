2d94af17d8e1812802e7077aba178fc9fb5e57d7
paolod <paolodellepiane@proton.me>
2022-11-09 18:18:16 +0100

    update ash.json uri

--- scripts/release-win.ps1
+++ scripts/release-win.ps1
@@ -13,5 +13,5 @@ Compress-Archive ash.exe ash-$version-win-x64.zip
 (Get-FileHash .\ash-$version-win-x64.zip).Hash > .\ash-$version-win-x64.zip.sha256
 gh release create v$version --generate-notes .\ash-$version-win-x64.zip .\ash-$version-win-x64.zip.sha256
 & $env:UserProfile\scoop\apps\scoop\current\bin\checkver.ps1 ash . -Update
-cat ash.json | gh gist edit a059f56c8026d8581add2bc29f80f144 -f ash.json -
+cat ash.json | gh gist edit 448ec9b86bcdf97faa1d7a3cd9d03d73 -f ash.json -
 cd $initialdir
--- scripts/update-scoop-manifest.ps1
+++
@@ -1,2 +0,0 @@
-& $env:UserProfile\scoop\apps\scoop\current\bin\checkver.ps1 ash . -Update
-cat ash.json | gh gist edit 448ec9b86bcdf97faa1d7a3cd9d03d73 -f ash.json -
